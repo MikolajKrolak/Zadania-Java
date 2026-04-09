dd41e9609b76cfdb4bd8d90b2f25471c7c145b42
MikolajKrolak <s35908@pjwstk.edu.pl>
2026-04-09 23:34:14 +0200

    Delete Zadania - 2 directory

--- Zadania - 2/Zadanie 1/src/Card.java
+++
@@ -1,9 +0,0 @@
-public enum Card {
-
-    KIER(1), PIK(2), TREFL(3), KARO(4);
-
-    public static final String[] wartosc = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Walet", "Dama", "Król", "As"};
-
-    Card(int id) {
-    }
-}
